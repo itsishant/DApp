ee7932235b97a19b32072daf831bc7ccd633856a
Ishant <modakmania@gmail.com>
2026-04-05 21:16:51 +0530

    Update landing.tsx

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -4,6 +4,7 @@ export const Landing = () => {
       <div>
         <h1 className="text-6xl font-extrabold text-neutral-300 tracking-wide mb-24">
           💦 DAPP 
+
           <span className="text-green-600">A</span>
           {""} DAPP AIRDROP 💦
         </h1>
